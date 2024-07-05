629b33f502c8faaf16d80b310afa75be9d7cf585
Rafael Oliveira <162510592+VesoliRafael@users.noreply.github.com>
2024-07-05 18:04:11 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/Contador/Contador.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Contador;
+
+/**
+ *
+ * @author alves
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner (System.in); 
+       System.out.println("Digite o primeiro parametro: ");
+            int parametroUm = scanner.nextInt();
+            System.out.println(" ");
+
+       System.out.println("Digite o segundo parametro: ");
+            int parametroDois = scanner.nextInt();
+            System.out.println(" ");
+
+       try {
+            contar(parametroUm, parametroDois);
+       } catch(ParametrosInvalidosException e){
+            System.out.println(e.getMessage());
+    
+       }
+    }
+
+    static void contar(int parametroUm, int parametroDois)throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro.");
+        }
+        
+        int contagem = parametroDois - parametroUm;
+
+        for(int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo o numero " + i);
+
+        }   
+
+    }
+
+    public static class ParametrosInvalidosException extends Exception {
+        public ParametrosInvalidosException (String mensagem){
+            super(mensagem);
+        }
+    }
+}
